b204ea81c808d7ccc51f301d61e631e4d1ba2bf0
zxyle <zxyful@gmail.com>
2020-08-30 22:33:47 +0800

    add admin package and docs

---
+++ app/admin/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Author: Zheng <zxyful@gmail.com>
+# Date: 2020-08-30
+# Desc:
+
+from flask import Blueprint
+
+admin_blue = Blueprint('admin', __name__)
+
+from . import views
